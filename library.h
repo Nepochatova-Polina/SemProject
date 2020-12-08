898eda626d5447d551a6ff863d234518e414adf1
Nepochatova Polina <nepochatova.polina@unicyb.kiev.ua>
2020-12-08 10:11:42 +0200

    new functions

--- library.h
+++ library.h
@@ -5,37 +5,37 @@
 
 using namespace std;
 
-static int *stringToGray(int number, int inputString[]);
+ int *stringToGray(int number, int inputString[]);
 
-static int NOD(int a, int b);
+ int NOD(int a, int b);
 
-static int NOK(int a, int b);
+ int NOK(int a, int b);
 
-static std::vector<int> factorize(int x);
+ std::vector<int> factorize(int x);
 
 int jacobi(int a, int n);
 
-static string ArmstrongNumber(int number);
+ string ArmstrongNumber(int number);
 
-static double Lagranz(double **matrix, int number);
+ double Lagranz(double **matrix, int number);
 
-static string ThreeOfPif();
+ string ThreeOfPif();
 
-static std::vector<int> find_dividers(int x);
+ std::vector<int> find_dividers(int x);
 
-static bool is_prime(int x);
+ bool is_prime(int x);
 
-static int AriphmeticSum(int n, int res);
+ int AriphmeticSum(int n, int res);
 
-static int GeometrySum(int n, int k);
+ int GeometrySum(int n, int k);
 
-static string naturNum(int n);
+ string naturNum(int n);
 
-static int BellNum(int n, int p);
+ int BellNum(int n, int p);
 
-static int ChineeseTheo(int *a, int *n, int *m, int *mi, int i, int size);
+ int ChineeseTheo(int *a, int *n, int *m, int *mi, int i, int size);
 
-static string CarperRes(int x);
+ string CarperRes(int x);
 
 double factorial(int num);
 #endif //LIBRARY_LIBRARY_H
